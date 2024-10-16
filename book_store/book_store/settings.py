342bf04602cb96ed4bb99e8beebc1719a53c8c05
mertwithamouth <yilmazakin93@gmail.com>
2024-10-16 16:59:38 +0300

    Queries has been lerned and templates has been started to code

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'book_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR /"book_outlet"/"templates",
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
