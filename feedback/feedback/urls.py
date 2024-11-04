2c9af3691b1de4875bd6c5cd2fb897c4b6620493
mertwithamouth <yilmazakin93@gmail.com>
2024-11-04 22:06:46 +0300

    .

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
-    path('profile', include('profiles.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,11 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.edit import View
+from .forms import ProfileForm
 # Create your views here.
 
-
+def store_file(file):
+    with open('temp/image.jpg', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 class CreateProfileView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'profiles/create_profile.html')
+        form = ProfileForm()
+        return render(request, 'profiles/create_profile.html',
+                      {'form': form})
 
     def post(self, request, *args, **kwargs):
-            pass
+            submitted_form=ProfileForm(request.POST, request.FILES)
+            if submitted_form.is_valid():
+                store_file(request.FILES['image'])
+                return HttpResponseRedirect('/profiles/create-profile')
+            return render(request, 'profiles/create_profile.html',
+                          {'form': submitted_form})
